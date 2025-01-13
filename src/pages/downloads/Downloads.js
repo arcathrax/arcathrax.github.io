867b2758229f42079c6e398c151762a08330d09f
Ivan <arcathrax@proton.me>
2025-01-13 14:21:33 +0100

    Update Downloads.js

--- src/pages/downloads/Downloads.js
+++ src/pages/downloads/Downloads.js
@@ -1,60 +1,44 @@
 import Navbar from "../../components/Navbar/Navbar";
 import Footer from "../../components/Footer/Footer";
 import "./Downloads.css";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
+function downloads() {
+  return (
+    <div>
+      <Navbar />
 
-function downloads(){
-    return(
-        <div>
-            <Navbar/>
+      <div className={"aboutmeMainDiv"}>
+        <h1>downloads</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Type</th>
+              <th>Download</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>exodist</td>
+              <td>audio plugin</td>
+              <td>
+                <Link
+                  to={
+                    "https://github.com/arcathrax/ExoDist/releases/download/v0.1.0/ExoDist.vst3.zip"
+                  }
+                >
+                  link
+                </Link>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
 
-            <div className={"aboutmeMainDiv"}>
-                <h1>
-                    downloads
-                </h1>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Type</th>
-                            <th>Download</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>
-                                ExoDist
-                            </td>
-                            <td>
-                                audio plugin
-                            </td>
-                            <td>
-                                <Link to={"https://github.com/arcathrax/ExoDist/releases/download/v0.1.0/ExoDist.vst3.zip"}>
-                                    link
-                                </Link>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                quora
-                            </td>
-                            <td>
-                                serum bank
-                            </td>
-                            <td>
-                                <Link to={"https://arcathrax.gumroad.com/l/quora"} target={"blank"}>
-                                    link
-                                </Link>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-
-            <Footer/>
-        </div>
-    );
+      <Footer />
+    </div>
+  );
 }
 
-export default  downloads;
+export default downloads;
